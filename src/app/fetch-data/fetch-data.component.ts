277cec6f7be8d0079800976da5fabf8129e31076
Allie <56135023+ahollenbaugh@users.noreply.github.com>
2023-02-22 23:37:52 -0800

    Copy 'n' paste stuff from ASP .NET/Angular app into this Angular-only app

---
+++ src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html',
+  styleUrls: ['./fetch-data.component.css']
+})
+export class FetchDataComponent {
+  public forecasts: WeatherForecast[] = [];
+  baseUrl = 'https://localhost:7232/';
+  
+  constructor(http: HttpClient){ // WITH dependency injection (without, you would write http=new HttpClient)
+    // YOU don't instantiate, the LIBRARY does.
+    // gets executed/invoked when class is instantiated
+    http.get<WeatherForecast[]>(this.baseUrl + 'weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
